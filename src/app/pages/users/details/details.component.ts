6e218ca9790edbbc34135a950f6446b460acad60
unknown <dleroux63160@gmail.com>
2023-11-10 13:49:56 +0100

    add getById User

--- src/app/pages/users/details/details.component.ts
+++ src/app/pages/users/details/details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../../models/user.model";
+import {UserService} from "../../../services/user/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  user$?: Promise<User>
+
+  constructor(private userService: UserService, private router: Router, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+
+    const id = this.route.snapshot.paramMap.get('id')
+    if (id) {
+      this.user$ = this.userService.getById(parseInt(id))
+    } else {
+      this.router.navigateByUrl('/not-found')
+    }
+  }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -32,9 +32,25 @@ export class UserService {
     })
     const options = {headers}
     const obsHttp$ = this.http
-      .get<UserHttp[]>(`${this.fullBaseUrlApi}/`,options)
+      .get<UserHttp[]>(`${this.fullBaseUrlApi}/`, options)
       .pipe(
-        map((usersHttp : UserHttp[])=>usersHttp.map((userHttp: UserHttp)=> User.mapperUserHttpToUser(userHttp)))
+        map((usersHttp: UserHttp[]) => usersHttp.map((userHttp: UserHttp) => User.mapperUserHttpToUser(userHttp)))
+      )
+
+    return firstValueFrom(obsHttp$)
+  }
+
+  getById(id: number): Promise<User> {
+    const token = this.authService.token as string
+    console.log(token)
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    const options = {headers}
+    const obsHttp$ = this.http
+      .get<UserHttp>(`${this.fullBaseUrlApi}/${id}`, options)
+      .pipe(
+        map((userHttp: UserHttp) => User.mapperUserHttpToUser(userHttp))
       )
 
     return firstValueFrom(obsHttp$)
